e8e9271f9bd882ffc100dcc2e7ba4faeba998604
Taavi <tk.taavi@gmail.com>
2020-11-04 15:02:34 +0200

    Raamide paika panemine

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 
 
-
 yf.pdr_override() # <== that's all it takes :-)
 
 # download dataframe
@@ -16,18 +15,31 @@ data = pdr.get_data_yahoo("AAPL", start="2020-01-01", end="2020-10-29").to_csv("
 
 
 print(data)
-root = tk.Tk()
+raam = tk.Tk()
 
-root.geometry("1000x500")
-root.pack_propagate(0)
-root.resizable(0, 0)
+raam.geometry("1300x800")
+raam.pack_propagate(0)
+raam.resizable(0, 0)
 # This is the frame for the Treeview
-frame1 = tk.LabelFrame(root, text="AAPL data")
-frame1.place(height=350, width=750)
 
+frame1 = tk.LabelFrame(raam, text="AAPL data")
+frame1.place(x=300, y=100, height=350, width=750)
+frame2 = tk.LabelFrame(raam, text="Lisainfo")
+frame2.place(height = 1300, width = 300)
+frame3 = tk.LabelFrame(raam, text="Aktsiad")
+frame3.place(x=1050,y=0,height = 1300, width = 250)
 
+variable = tk.StringVar(raam)
+variable.set("Päev") # default value
+
+w = tk.OptionMenu(raam, variable, "Päev", "Nädal", "Kuu", "Aasta")
+w.place(x=350, y = 60)
+
+l1 = tk.Entry(frame3,width = "200").pack()
+l2 = tk.Entry(frame3,width= "200").pack()
 f= open("info.csv")
 aktsiaInfo = []
+
 for line in f:
     info = line.strip().split(",")
     aktsiaInfo.append(info)
@@ -54,10 +66,12 @@ df1 = DataFrame(data1,columns=["Päevad","Hinnad"])
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 ax.plot(päevad,hinnad,color ="r")
+plt.tight_layout()
+
 ax.set_xticks([])
 
 graafik = FigureCanvasTkAgg(fig, frame1)
 graafik.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
 
 
-root.mainloop()
+raam.mainloop()
