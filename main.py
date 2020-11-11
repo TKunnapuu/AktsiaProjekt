3d339f912225cdcd4ae485d8ac3eab8ce3b3e7e7
Taavi <tk.taavi@gmail.com>
2020-11-11 22:54:25 +0200

    x telje muutmise algus faili salvestamine

--- main.py
+++ main.py
@@ -8,54 +8,75 @@ from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 
+# esimese aktsia kuvamine programmi käivitudes
+# x-telg korda
+# informatsiooni juurde
+# kujundus ilusamaks
+# kood korda
+# info automaatne uuendamine
+# alerti tegemine
+# hiir graafikul funktsionaalsus
+
 
 def onClick(event):
     aktsia = event.widget.get()
-    if(len(aktsia) < 1):
+    if (len(aktsia) < 1):
         print("klikkasid tühja")
     elif aktsia != frame1.cget("text"):
         print("klikkasid", aktsia)
         vaadeldavAktsia = aktsia
         vahetaGraafik(vaadeldavAktsia)
 
+
 def caps(event):
     entryStringVars[entries.index(event.widget)].set(event.widget.get().upper())
 
+
 def ajavahetus(event):
     print("Vahetasid ajavahemiku")
-    print("Hetkel vaatan:",frame1.cget("text"))
+    print("Hetkel vaatan:", frame1.cget("text"))
     vahetaGraafik(frame1.cget("text"))
 
+
 def salvesta(event):
-    print("Head aega")
     vaadeldavAktsia = event.widget.get()
-    vahetaGraafik(vaadeldavAktsia)
-    print(vaadeldavAktsia)
-    raam.focus_set()
+    f = open("nimekiri.txt","w")
+
+    for u in range(0,len(entryStringVars)):
+        f.write(entryStringVars[u].get()+"\n")
+    f.close()
+
+    if (len(vaadeldavAktsia) < 1):
+        print("Tühi")
+    else:
+        vahetaGraafik(vaadeldavAktsia)
+        print(vaadeldavAktsia)
+        raam.focus_set()
+
 
 def vahetaGraafik(vaadeldavAktsia):
     print(variable.get())
-    frame1.configure(text = vaadeldavAktsia)
+    frame1.configure(text=vaadeldavAktsia)
     täna = datetime.today()
     period = ""
     interval = ""
-    if(variable.get() == "Päev"):
+    if (variable.get() == "Päev"):
         period = "1d"
-        interval = "15m"
-    elif(variable.get()== "Nädal"):
+        interval = "5m"
+    elif (variable.get() == "Nädal"):
         period = "5d"
-        interval = "30m"
-    elif(variable.get()=="Kuu"):
+        interval = "15m"
+    elif (variable.get() == "Kuu"):
         period = "1mo"
         interval = "90m"
-    elif(variable.get()=="Aasta"):
+    elif (variable.get() == "Aasta"):
         period = "1y"
         interval = "5d"
     yf.pdr_override()
 
     data = yf.download(  # or pdr.get_data_yahoo(...
         # tickers list or string as well
-        tickers = vaadeldavAktsia,
+        tickers=vaadeldavAktsia,
 
         # use "period" instead of start/end
         # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
@@ -83,9 +104,6 @@ def vahetaGraafik(vaadeldavAktsia):
         # (optional, default is True)
         threads=True,
 
-        # proxy URL scheme use use when downloading?
-        # (optional, default is None)
-        proxy=None
     ).to_csv("info.csv")
     f = open("info.csv")
     aktsiaInfo = []
@@ -94,6 +112,7 @@ def vahetaGraafik(vaadeldavAktsia):
         info = line.strip().split(",")
         aktsiaInfo.append(info)
 
+    f.close()
     print(aktsiaInfo)
 
     päevad = []
@@ -105,19 +124,44 @@ def vahetaGraafik(vaadeldavAktsia):
         päevad.append(p[0])
         hinnad.append(round(float(p[4]), 5))
 
+    kohandatud_päevad = []
+
+    if (variable.get() == "Päev"):
+        for el in päevad:
+            kohandatud_päevad.append((el.split(" ")[1]).split("-")[0][:-3])
+    else:
+        #Ajutine
+        kohandatud_päevad = päevad
+
+       # elif (variable.get() == "Nädal"):
+       #     for el in päevad:
+       #         el = el.split(" ")[1]
+       # elif (variable.get() == "Kuu"):
+       #     for el in päevad:
+       #         el = el.split(" ")[1]
+       # elif (variable.get() == "Aasta"):
+       #     for el in päevad:
+       #         el = el.split(" ")[1]
+
     print(päevad)
     print(hinnad)
+    print(len(hinnad))
 
-    data1 = {"Päevad": päevad,
+    data1 = {"Päevad": kohandatud_päevad,
              "Hinnad": hinnad}
 
     df1 = DataFrame(data1, columns=["Päevad", "Hinnad"])
 
-    fig = plt.figure(figsize=(15,2))
+    fig = plt.figure(figsize=(15, 2))
+    plt.margins(x=0)
     ax = fig.add_subplot(1, 1, 1)
-    ax.plot(päevad, hinnad, color="r")
+    ax.plot(kohandatud_päevad, hinnad, color="r")
     plt.grid()
-    ax.set_xticks(päevad)
+    if (variable.get() == "Päev"):
+        ax.set_xticks(["10:00", "12:00", "14:00"])
+    else:
+        ax.set_xticks([])
+
     for widget in frame1.winfo_children():
         widget.destroy()
     graafik = FigureCanvasTkAgg(fig, frame1)
@@ -133,18 +177,15 @@ raam.resizable(0, 0)
 frame1 = tk.LabelFrame(raam, text="AAPL data")
 frame1.place(x=300, y=100, height=350, width=750)
 frame2 = tk.LabelFrame(raam, text="Lisainfo")
-frame2.place(height = 1300, width = 300)
+frame2.place(height=1300, width=300)
 frame3 = tk.LabelFrame(raam, text="Aktsiad")
-frame3.place(x=1050,y=0,height = 1300, width = 250)
+frame3.place(x=1050, y=0, height=1300, width=250)
 
 variable = tk.StringVar(raam)
 variable.set("Päev")
 
-
 w = tk.OptionMenu(raam, variable, "Päev", "Nädal", "Kuu", "Aasta", command=ajavahetus)
-w.place(x=350, y = 60)
-
-
+w.place(x=350, y=60)
 
 entries = []
 entryStringVars = []
@@ -152,7 +193,7 @@ f = open("nimekiri.txt")
 
 for i in range(25):
     var = tk.StringVar()
-    l = tk.Entry(frame3, textvariable = var)
+    l = tk.Entry(frame3, textvariable=var)
     l.bind("<Button-1>", onClick)
     l.bind("<Return>", salvesta)
     l.bind("<KeyRelease>", caps)
